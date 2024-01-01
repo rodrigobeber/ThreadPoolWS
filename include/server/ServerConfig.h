c4fb3f880950d10ea23d425dff14b9a02bdb5681
Rodrigo Luiz Beber <rodrigobeber@gmail.com>
2024-01-01 09:08:59 -0300

    n threads = cpu cores qtd

--- include/server/ServerConfig.h
+++ include/server/ServerConfig.h
@@ -4,7 +4,7 @@
 
 struct ServerConfig {
     size_t port = 8080;
-    size_t poolSize = 4;
+    size_t poolSize = std::thread::hardware_concurrency();
     size_t maxQueuedConnections = 100;
     void parseArguments(int argc, char* argv[]);
 };
